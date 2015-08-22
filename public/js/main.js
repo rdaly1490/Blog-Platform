f09d2059a4a264144f59165df57c5aeb5f3199e3
Rob <rdaly1490@gmail.com>
2015-08-22 18:53:29 -0500

    backbone router hooked up

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,40 @@
 var React = require('react');
+var Backbone = require("backbone");
+Backbone.$ = require("jquery");
 
-console.log('application running');
+var App = Backbone.Router.extend({
+	routes: {
+		"": "login",
+		"login":"login",
+		"register":"register",
+		"home": "home"
+	},
+	login: function() {
+		React.render(
+			<div>
+				<h1>Login Page</h1>
+			</div>,
+			document.getElementById("container")
+		);
+	},
+	register: function() {
+		React.render(
+			<div>
+				<h1>Register Page</h1>
+			</div>,
+			document.getElementById("container")
+		);
+	},
+	home: function() {
+		React.render(
+			<div>
+				<h1>Home Page</h1>
+			</div>,
+			document.getElementById("container")
+		);
+	}
+
+});
+
+var myRouter = new App();
+Backbone.history.start();
